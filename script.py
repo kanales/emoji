3178fbb13972a28d212f57ee1b640d78779435b5
kanales <icanalma7@alumnes.ub.edu>
2017-04-01 22:41:44 +0200

    first repository

---
+++ script.py
@@ -0,0 +1,8 @@
+_ = [].__add__.__name__[2] + chr(46) + (lambda: 0).__class__.__name__[3] + [].__class__.__name__[2] + [].remove.__name__[4]
+__ = __import__(_[2:])
+___ = getattr(__, dir(__)[24])
+____ = {k: v for k,v in ___(open(_, [].__repr__.__name__[2]))}
+_____ = lambda s: str().join(map(lambda  l: ____[l], s.upper()))
+
+while 1:
+    print(_____(str(raw_input())))
